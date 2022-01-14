d798a2d672c143b5536ac9d95a4d30e10485227c
Chriss <christopher.vera@epn.edu.ec>
2022-01-14 14:19:33 -0500

    Sprint0

---
+++ app/Models/Jail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jail extends Model
+{
+    use HasFactory;
+
+    public function ward(){
+        return $this->belongsTo(Ward::class);
+    }
+
+    public function users(){
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+    public function image(){
+        return $this->morphOne(Image::class,'imageable');
+    }
+}
