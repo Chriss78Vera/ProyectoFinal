0e30add04007f50f8db636c4539850f19f26740f
Chriss <christopher.vera@epn.edu.ec>
2022-01-15 21:33:34 -0500

    Sprint1

--- database/seeders/WardSeeder.php
+++ database/seeders/WardSeeder.php
@@ -11,17 +11,9 @@ class WardSeeder extends Seeder
 
     public function run()
     {
-
-
-        Ward::factory()->count(20)->create();
-
-
-        
+        Ward::factory()->count(20)->create();      
         $users_guards = User::where('role_id', 3)->get();
-        // dd($users_guards);
-        // dd(count($users_guards));
         $wards=Ward::all();
-        // Los guardias pueden estar en varios pabellones
         $wards->each(function($ward) use ($users_guards)
         {
             $ward->users()->attach($users_guards->shift(2));
